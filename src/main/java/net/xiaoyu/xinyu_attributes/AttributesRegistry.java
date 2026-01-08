e6a250a98903f786841d4e503c3ac74a8b502969
Xiaoyu_2009 <3551371733@qq.com>
2026-01-09 02:42:16 +0800

    添加了单次百分比伤害上限/重构了部分代码

--- src/main/java/net/xiaoyu/xinyu_attributes/AttributesRegistry.java
+++ src/main/java/net/xiaoyu/xinyu_attributes/AttributesRegistry.java
@@ -9,36 +9,41 @@ public class AttributesRegistry {
 
     // 火焰抗性
     public static final DeferredHolder<Attribute, Attribute> FIRE_RESISTANCE = ATTRIBUTES.register("fire_resistance",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.fire_resistance", 0.0, 0.0, 1.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".fire_resistance", 0.0, 0.0, 1.0)
     );
 
     // 冰冻抗性
     public static final DeferredHolder<Attribute, Attribute> FREEZE_RESISTANCE = ATTRIBUTES.register("freeze_resistance",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.freeze_resistance", 0.0, 0.0, 1.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".freeze_resistance", 0.0, 0.0, 1.0)
     );
 
     // 毒素抗性
     public static final DeferredHolder<Attribute, Attribute> POISON_RESISTANCE = ATTRIBUTES.register("poison_resistance",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.poison_resistance", 0.0, 0.0, 1.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".poison_resistance", 0.0, 0.0, 1.0)
     );
 
     // 凋零抗性
     public static final DeferredHolder<Attribute, Attribute> WITHER_RESISTANCE = ATTRIBUTES.register("wither_resistance",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.wither_resistance", 0.0, 0.0, 1.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".wither_resistance", 0.0, 0.0, 1.0)
     );
 
     // 声波抗性
     public static final DeferredHolder<Attribute, Attribute> SONIC_RESISTANCE = ATTRIBUTES.register("sonic_resistance",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.sonic_resistance", 0.0, 0.0, 1.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".sonic_resistance", 0.0, 0.0, 1.0)
     );
 
     // 单次伤害上限
     public static final DeferredHolder<Attribute, Attribute> SINGLE_DAMAGE_LIMIT = ATTRIBUTES.register("single_damage_limit",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.single_damage_limit", 0.0, 0.0, Double.MAX_VALUE)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".single_damage_limit", 0.0, 0.0, Double.MAX_VALUE)
+    );
+
+    // 单次百分比伤害上限
+    public static final DeferredHolder<Attribute, Attribute> SINGLE_PERCENTAGE_DAMAGE_LIMIT = ATTRIBUTES.register("single_percentage_damage_limit",
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".single_percentage_damage_limit", 0.0, 0.0, 1.0)
     );
 
     // 穿墙
     public static final DeferredHolder<Attribute, Attribute> PHASING = ATTRIBUTES.register("phasing",
-        () -> new RangedAttribute("attribute.name.xinyu_attributes.phasing", 0.0, 0.0, 0.0)
+        () -> new RangedAttribute("attribute.name."+ XinYuAttributes.MOD_ID +".phasing", 0.0, 0.0, 0.0)
     );
 }
--- src/main/java/net/xiaoyu/xinyu_attributes/mixin/LivingEntityMixin.java
+++ src/main/java/net/xiaoyu/xinyu_attributes/mixin/LivingEntityMixin.java
@@ -10,17 +10,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
+    
     @ModifyVariable(method = "hurt", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private float modifyDamageAmount(float originalAmount, DamageSource source) {
         LivingEntity entity = (LivingEntity) (Object) this;
 
         float amountAfterResistance = ResistanceUtil.applyResistance(entity, source, originalAmount);
-        double damageLimit = entity.getAttributeValue(AttributesRegistry.SINGLE_DAMAGE_LIMIT);
-        
+        double fixedDamageLimit = entity.getAttributeValue(AttributesRegistry.SINGLE_DAMAGE_LIMIT);
+        double percentageDamageLimit = entity.getAttributeValue(AttributesRegistry.SINGLE_PERCENTAGE_DAMAGE_LIMIT);
+
+        if (!entity.getAttribute(AttributesRegistry.SINGLE_DAMAGE_LIMIT).getModifiers().isEmpty() &&
+            fixedDamageLimit >= 0 && amountAfterResistance > fixedDamageLimit) {
+            return (float) fixedDamageLimit;
+        }
+
         if (!entity.getAttribute(AttributesRegistry.SINGLE_DAMAGE_LIMIT).getModifiers().isEmpty() && 
-            damageLimit >= 0 && amountAfterResistance > damageLimit) {
-            return (float) damageLimit;
+            fixedDamageLimit > entity.getMaxHealth()) {
+            entity.setHealth(0);
+        }
+        
+        if (!entity.getAttribute(AttributesRegistry.SINGLE_PERCENTAGE_DAMAGE_LIMIT).getModifiers().isEmpty() && 
+            percentageDamageLimit >= 0 && percentageDamageLimit <= 1) {
+            float maxDamageByPercentage = (float) (entity.getMaxHealth() * percentageDamageLimit);
+            
+            if (amountAfterResistance > maxDamageByPercentage) {
+                amountAfterResistance = maxDamageByPercentage;
+            }
         }
         
         return amountAfterResistance;
@@ -32,9 +47,19 @@ public abstract class LivingEntityMixin {
         
         float amountAfterResistance = ResistanceUtil.applyResistance(entity, source, amount);
         double damageLimit = entity.getAttributeValue(AttributesRegistry.SINGLE_DAMAGE_LIMIT);
+        double percentageDamageLimit = entity.getAttributeValue(AttributesRegistry.SINGLE_PERCENTAGE_DAMAGE_LIMIT);
 
         if (!entity.getAttribute(AttributesRegistry.SINGLE_DAMAGE_LIMIT).getModifiers().isEmpty() && 
-            damageLimit <= 0.0 || amountAfterResistance <= 0.0) {
+            damageLimit <= 0) {
+            cir.cancel();
+        }
+
+        if (!entity.getAttribute(AttributesRegistry.SINGLE_PERCENTAGE_DAMAGE_LIMIT).getModifiers().isEmpty() && 
+            percentageDamageLimit <= 0) {
+            cir.cancel();
+        }
+
+        if (amountAfterResistance <= 0) {
             cir.cancel();
         }
     }
